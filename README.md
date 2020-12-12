a8e694417a51d5296827bdd3d5cac9b18f7ae162
Sifrisky <astrid.amg@gmail.com>
2020-12-12 00:04:52 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+## Consumiendo API con Javascript
+
+# Preview
+
+![](docs/screenshot.jpeg)
+
+# Comenzamos 🚀
+
+Acá construí una aplicación de escritorio sencilla en la cual vamos a consumir información traída desde OMDBAPI.  
+Hice uso de Axios para realizar las peticiones y utilizamos **Vue.JS** como framework de JavaScript.
+
+Para usar esta API es necesario ir al apartadp **API Key**
+http://www.omdbapi.com/apikey.aspx y colocar tu correo electronico.
+
+
+# Instalaciones:
+
+USING NPM:
+``
+npm install axios
+``
+
+CDN:
+``
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+``
+
+# Vue.js
+
+https://vuejs.org/v2/guide/
+
+CDN:
+
+<script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
+
+# Bootstrap
+
+https://getbootstrap.com/
+
+components/navbar
+
+Como queremos buscar por titulos usamos la "s" es decir, /?s=
---
+++ js/scripts.js
@@ -0,0 +1,88 @@
+var app = new Vue({
+    el: "#app",
+    data() {
+      return {
+          query: null,
+          results: null,
+          apikey: '67b2bb72'
+      }
+    },
+    created() {
+      this.getMovies();
+    },
+    methods: {
+      getMovies() {
+        axios.get(`http://www.omdbapi.com/?s=${ this.query }&apikey=${ this.apikey }`)
+          .then(response => {
+            console.log(response.data.Search)
+  
+            results = response.data.Search
+  
+            let html = '';
+  
+            results.forEach(result => {
+              html += `
+                <div class="row">
+                  <div class="col-lg-12 col-sm-12 col-md-6" movies>
+                  <div class="card style="width: 18rem;">
+                    <img src="${ result.Poster }" class="card-image-top img-fluid">
+                    <div class="card-body">
+                      <h4 class="card-title">${result.Title }</h4>
+                      <p class="card-text">Year: ${ result.Year }</p>
+                    </div>
+                  </div>
+                  </div>
+                </div>
+              `
+            });
+  
+            document.getElementById('movies').innerHTML = html;
+  
+          })
+      }
+    }
+  })
+
+/*var app= new Vue({
+el: "#app",
+data(){
+    return {
+        query: null,
+        results: null,
+        apikey:'67b2bb72'
+    }
+},
+    methods: {
+         getMovies(){
+        //console.log('funciona');
+            axios.get(`http://www.omdbapi.com/?s=${this.query}&apikey=${this.apikey}`)
+                .then(response => {
+                console.log(response.data.Search)
+
+                    results = response.data.Search
+            //traer info Api
+            let html = '';
+
+            results.forEach(result => {
+                html += `
+                <div class="row>
+                    <div class="col-lg-4" movies>
+                        <div class="card style="width: 18rem;"> 
+                                <img src="${result.Poster}" class="card-image-top img-fluid">
+                                <div class="card-body">
+                                <h4 class="card-title">${result.title} </h4>
+                                <p class="card-text">Year; ${result.year} </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                `
+                
+            });
+            
+            document.getElementById('movies').innerHTML = html;
+            })
+        }
+    }
+})*/
